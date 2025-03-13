431fe54749020cbb6f8c82c7fbdc3f24b8d77838
Peter Sargent <peter.sargent@snhu.edu>
2025-03-13 16:04:06 -0400

    Added randomization

--- syntheticdata.py
+++ syntheticdata.py
@@ -2,7 +2,8 @@ import os
 import random
 from PIL import Image
 
-def overlay_png_on_images(base_folder, overlay_path, output_folder, baseimgx, baseimgy, scalemin, scalemax, class_id):
+
+def overlay_png_on_images(base_folder, output_folder, baseimgx, baseimgy, scalemin, scalemax, class_id):
     # Create folders for images and annotations if they don't exist
     images_folder = os.path.join(output_folder, "images")
     if not os.path.exists(images_folder):
@@ -11,33 +12,44 @@ def overlay_png_on_images(base_folder, overlay_path, output_folder, baseimgx, ba
     annotations_folder = os.path.join(output_folder, "labels")
     if not os.path.exists(annotations_folder):
         os.makedirs(annotations_folder)
-    
+
     base_width, base_height = baseimgx, baseimgy  # Base image dimensions
-    overlay_img = Image.open(overlay_path).convert("RGBA")
-    
-    for filename in os.listdir(base_folder)[:100]:
+
+    # Create overlay images
+    overlay_path_array = [r"pfm-image01.png", r"pfm-image02.png", r"pfm-image03.png",
+                          r"pfm-image04.png", r"pfm-image05.png"]
+    overlay_image_array = []
+    for file_name in overlay_path_array:
+        overlay_image = Image.open(file_name).convert("RGBA")
+        overlay_image_array.append(overlay_image)
+
+    for filename in os.listdir(base_folder)[:1000]:
         if filename.lower().endswith(('png', 'jpg', 'jpeg')):
             base_img_path = os.path.join(base_folder, filename)
             base_img = Image.open(base_img_path).convert("RGBA")
-            
-            for i in range(5):  
+
+            print("Creating images based on " + filename)
+            for i in range(5):
                 new_img = base_img.copy()
-                
+
+                # Pick a random overlay image
+                overlay_img = random.choice(overlay_image_array)
+
                 # Determine random scale % of base image size, use 0.01
                 scale_factor = random.uniform(scalemin, scalemax)
                 new_width = int(base_width * scale_factor)
                 new_height = int(overlay_img.height * (new_width / overlay_img.width))
                 resized_overlay = overlay_img.resize((new_width, new_height))
-                
+
                 # Determine random position within bounds
                 max_x = base_width - new_width
                 max_y = base_height - new_height
                 x_offset = random.randint(0, max_x)
                 y_offset = random.randint(0, max_y)
-                
+
                 # Overlay PNG onto image
                 new_img.paste(resized_overlay, (x_offset, y_offset), resized_overlay)
-                
+
                 # Save modified image
                 image_output_path = os.path.join(images_folder, f"{os.path.splitext(filename)[0]}_overlay_{i}.png")
                 with open(image_output_path, "wb") as f:
@@ -57,11 +69,10 @@ def overlay_png_on_images(base_folder, overlay_path, output_folder, baseimgx, ba
                     # class_id is 0 by default, we may need to rework this part - PS
 
 
-base_folder = r"original_images"  # Folder where original images are located
-overlay_path = r"pfm1-landmine-image01.png"  # Image that you'd like to overlay
-output_folder = r"synth_dataset"  # Folder where new images should be placed
+base_directory = r"original_images"  # Folder where original images are located
+output_directory = r"synth_dataset"  # Folder where new images should be placed
 # Change values based on your datasets
-overlay_png_on_images(base_folder, overlay_path, output_folder, 1280, 720, 0.07, 0.25, 0)
+overlay_png_on_images(base_directory, output_directory, 1280, 720, 0.07, 0.25, 0)
 
 # Images sourced from https://www.kaggle.com/datasets/vinayakshanawad/weedcrop-image-dataset
 # and from https://zenodo.org/records/7951745
